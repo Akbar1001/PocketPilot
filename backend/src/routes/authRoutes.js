837e85f8bc1fd044b11e0f200e8fc7d51ed344b1
Akbar <akbar.code20@gmail.com>
2026-08-19 00:02:19 +0530

    Added middleware for authorize validation

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const authenticate = require("../middleware/authMiddleware");
+
 
 const {
     registerUser,
@@ -9,5 +11,13 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me", authenticate, (req, res) => {
+    res.json({
+        success: true,
+        message: "You are authenticated",
+        userId: req.user.id
+    });
+});
+
 
 module.exports = router;
