c71c237a99669d043719e1decab31c7f0e9cd28b
Akbar <akbar.code20@gmail.com>
2026-08-18 18:10:10 +0530

    Fixed Server error

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,4 +8,12 @@ app.use(cors());  //Allows frontend to communicate with backend
 app.use(helmet()); //Helmet adds security-related HTTP headers.
 app.use(express.json());  //express.json() cuz -> Express needs to parse that JSON request body
 
+app.get("/api/health", (req, res) => {
+    res.json({
+        success: true,
+        message: "PocketPilot API is running"
+    });
+});
 
+
+module.exports = app;
