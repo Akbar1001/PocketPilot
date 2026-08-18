0153f8a28d111ecd28fb1cd1aae3169afd869a36
Akbar <akbar.code20@gmail.com>
2026-08-18 18:27:22 +0530

    server and Db running correctly

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
+require("dotenv").config();
+
 const app = require("./src/app");
 
 const PORT = process.env.PORT || 8000;
+const pool=require("./src/config/database.js");
 
-app.listen(PORT, () => {
-    console.log(`PocketPilot API running on port ${PORT}`);
-});
+const startserver= async () => {
+    try{
+        await pool.query("SELECT NOW()");
+
+        console.log("DB connected Successfully");
+
+        app.listen(PORT,()=>{
+            console.log(`PocketPilot API running on port ${PORT}`);
+            
+        }) 
+    }catch(error){
+        console.error("Database connection failed:", error.message);
+        process.exit(1);
+    }
+}
+
+startserver();
